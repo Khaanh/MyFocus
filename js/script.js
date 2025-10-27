def27bf593c7553bd78501b7025603d68a75d313
Khaanh <khanh13nguyen13@gmail.com>
2025-10-27 22:24:34 +0100

    WIP: add random number generator function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("init");
+const quizBlock = document.querySelector("#quizBlock");
+const numbers = Array.from({ length: 100 }, (_, i) => i);
+
+const val1 = Math.floor(Math.random() * numbers.length) + 1;
+const val2 = Math.floor(Math.random() * numbers.length) + 1;
+const val3 = Math.floor(Math.random() * numbers.length) + 1;
+const val4 = Math.floor(Math.random() * numbers.length) + 1;
+
+quizBlock.innerHTML = `${val1} + ${val2}`;
