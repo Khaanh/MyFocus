8e8707c07cd55bf0911c53d9693588c3b05cb79e
Khaanh <khanh13nguyen13@gmail.com>
2025-10-28 20:48:17 +0100

    feat: simple check answer statement

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const quizBlock = document.querySelector("#quizBlock");
+const quizResult = document.querySelector("#quizResult");
+const quizBtnCheck = document.querySelector("#quizBtnCheck");
+const answer = document.querySelector("#answer");
+
 const numbers = Array.from({ length: 100 }, (_, i) => i);
 
 const val1 = Math.floor(Math.random() * numbers.length) + 1;
@@ -6,4 +10,15 @@ const val2 = Math.floor(Math.random() * numbers.length) + 1;
 const val3 = Math.floor(Math.random() * numbers.length) + 1;
 const val4 = Math.floor(Math.random() * numbers.length) + 1;
 
+const correctAnswer = val1 + val2;
 quizBlock.innerHTML = `${val1} + ${val2}`;
+
+quizBtnCheck.addEventListener("click", () => {
+	if (Number(quizResult.value) === correctAnswer) {
+		console.log(true);
+		answer.textContent = `${quizResult.value}`;
+	} else {
+		console.log(false);
+		answer.textContent = `${quizResult.value}`;
+	}
+});
