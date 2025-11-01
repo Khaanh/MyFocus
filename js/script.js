c0647b48b170bda48f5bcbfd8cb4f6bd69c1df23
Khaanh <khanh13nguyen13@gmail.com>
2025-11-01 23:58:01 +0100

    feat: add correct/incorrect answer display with mobile styles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const quizBlock = document.querySelector("#quizBlock");
 const quizResult = document.querySelector("#quizResult");
 const quizBtnCheck = document.querySelector("#quizBtnCheck");
+const inCorrect = document.querySelector("#inCorrect");
+const correct = document.querySelector("#correct");
 const answer = document.querySelector("#answer");
 
 const numbers = Array.from({ length: 100 }, (_, i) => i);
 
 let val1, val2, correctAnswer;
+let correctCount = 1;
+let inCorrectCount = -1;
 
 function randomInt() {
 	val1 = Math.floor(Math.random() * numbers.length) + 1;
@@ -22,10 +26,12 @@ console.log(val1, val2);
 quizBtnCheck.addEventListener("click", () => {
 	if (Number(quizResult.value) === correctAnswer) {
 		console.log(true);
+		correct.textContent = `Correct answer: ${correctCount++}`;
 		answer.textContent = `${quizResult.value}`;
 		quizResult.value = "";
 	} else {
 		console.log(false);
+		inCorrect.textContent = `Incorrect answer: ${inCorrectCount--}`;
 		answer.textContent = `${quizResult.value}`;
 		quizResult.value = "";
 	}
